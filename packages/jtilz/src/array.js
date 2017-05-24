d8da4f331fb8786e74dd5465cae2801bc6acaf8f
Mark Penner <mpenner@nucleuslabs.com>
2017-05-24 16:30:22 -0700

    flatMap, thru, tap

--- packages/jtilz/src/array.js
+++ packages/jtilz/src/array.js
@@ -6,9 +6,9 @@ export const flatten = bindable(arrayOfArrays => Array.prototype.concat.call(...
 export const map = bindable((array,callback) => Array.prototype.map.call(array, callback));
 
 /**
- * Map + flatten
+ * Map and flatten
  */
-export const selectMany = bindable((array,callback) => array::map(callback)::flatten());
+export const flatMap = bindable((array, callback) => array::map(callback)::flatten());
 
 export const groupBy = bindable(function groupBy(array, grouper) {
     return array.reduce((acc,val,idx) => {
@@ -20,4 +20,11 @@ export const groupBy = bindable(function groupBy(array, grouper) {
         }
         return acc;
     }, Object.create(null));
+});
+
+export const thru = bindable((array,callback) => callback(array));
+
+export const tap = bindable((array,callback) => {
+    callback(array);
+    return array;
 });
