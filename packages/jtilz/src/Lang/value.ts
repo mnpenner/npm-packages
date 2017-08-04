d33d5cb5f8fb445673d7f2e087a8e8dc64e5e706
Mark Penner <mpenner@nucleuslabs.com>
2017-08-03 17:26:39 -0700

    Missing parens

--- packages/jtilz/src/Lang/value.ts
+++ packages/jtilz/src/Lang/value.ts
@@ -15,7 +15,7 @@ export function identity<T>(arg: T): T {
 /**
  * Unwraps a value. If passed a function, evaluates that function with the provided args. Otherwise, returns the value as-is.
  */
-export function value<T>(this: any, functionOrValue: (...args: any[]) => T|T, ...args: any[]): T {
+export function value<T>(this: any, functionOrValue: ((...args: any[]) => T)|T, ...args: any[]): T {
     return Type.isFunction(functionOrValue) ? functionOrValue.call(this, ...args) : functionOrValue;
 }
 
