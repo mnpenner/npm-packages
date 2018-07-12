b13f10266c5c91814876f6573a919b3b78ed8219
Mark Penner <mpenner@nucleuslabs.com>
2018-07-12 11:00:51 -0700

    Fixed up validate

--- packages/onemig/src/commands/validate.js
+++ packages/onemig/src/commands/validate.js
@@ -1,25 +1,5 @@
 import dump from '../dump';
-import {readDir, readJson} from '../util/fs';
-// import objHash from 'object-hash';
-// import napi, {dbNameMap} from '../napi';
-// import {InputOption} from '../console';
-import Path from 'path';
-// import db from '../db';
-// import {getDatabaseCollation, getDefaultStorageEngine, getStruct} from '../schema/struct';
-// import Ajv from 'ajv';
-// import tableSchema from '../table.schema.js';
-// import {omit} from '../util/object';
-// import {isNumber, isObject, isPlainObject} from '../util/types';
-// import {highlight} from 'cli-highlight';
-import {ciCompare, toIter} from '../util/array';
-// import Konsole from '../util/Konsole';
-// import Chalk from 'chalk';
-// import * as async from '../util/async';
-// import * as fs from '../util/fs';
-// import {addMany} from '../util/set';
-// import conn from '../db';
-// import Crypto from 'crypto';
-import Validator from '../schema/validator';
+import readSchema from '../schema/readSchema';
 
 export default {
     name: "validate",
@@ -36,33 +16,26 @@ export default {
     async execute(args, opts) {
         if(!args[0]) throw new Error("Please specify a directory to check");
 
-        const validator = Validator();
+
         const tableMap = new Map;
         let errorCount = 0;
+        
+        for(const inputDir of args) {
+            const tables = await readSchema(inputDir);
 
-        for(let dir of args) {
-            const tableFiles = (await readDir(Path.join(dir, 'tables'))).filter(f => f.endsWith('.json'));
-            tableFiles.sort(ciCompare);
-
-            for(let filename of tableFiles) {
-                const tbl = await readJson(filename);
-                const ajvErrors = validator.validate(tbl);
-                if(ajvErrors) {
-                    console.log(filename);
-                    dump(ajvErrors);
-                    ++errorCount;
-                }
+            for(let tbl of tables.values()) {
                 for(let {databases} of tbl.versions) {
                     for(let dbName of databases) {
                         const key = JSON.stringify([dbName,tbl.name]);
                         if(tableMap.has(key)) {
-                            console.log(`${dbName}.${tbl.name} already has a definition in ${tableMap.get(key)}; found another definition in ${filename}`)
+                            console.log(`${dbName}.${tbl.name} already has a definition in ${tableMap.get(key)}; found another definition in ${tbl.filename}`)
                             ++errorCount;
                         } else {
-                            tableMap.set(key,filename);
+                            tableMap.set(key,tbl.filename);
                         }
                     }
                 }
+
             }
         }
 
