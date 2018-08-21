a65c8c27a1ed764c73e2878597476e5ac0141e82
Mark Penner <mpenner@nucleuslabs.com>
2018-08-21 15:41:01 -0700

    Running code

---
+++ packages/react-combobox/webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: ['./src/index'],
+    mode: 'development',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    devtool: false,
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: ['awesome-typescript-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        alias: {
+            react: path.resolve(path.join(__dirname, './node_modules/react')),
+            'babel-core': path.resolve(
+                path.join(__dirname, './node_modules/@babel/core'),
+            ),
+        },
+    },
+    plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
+}
