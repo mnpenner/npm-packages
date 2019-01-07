e001260d379a1a3a6e0b6d36b9facf05cebb0ac0
Mark Penner <mark@mpen.ca>
2019-01-06 23:25:19 -0800

    Loading spinner

--- packages/crap-app/template/src/components/ActionButton.tsx
+++ packages/crap-app/template/src/components/ActionButton.tsx
@@ -1,11 +1,5 @@
 import * as React from 'react';
 
-export default function ActionButton({onClick,...props}: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) {
-    if(onClick) {
-        props.onClick = function onClickEvent(ev) {
-            ev.preventDefault();
-            return onClick(ev);
-        }
-    }
-    return <button {...props}/>
+export default function ActionButton(props: React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>) {
+    return <button type="button" {...props}/>
 }
