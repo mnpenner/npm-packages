8312da4473a2e8e7e648e36e253bc629fbfca5ca
Mark Penner <code@mpen.ca>
2019-12-17 20:55:41 -0800

    `ctslib` release

---
+++ packages/create-tslib/template/src/index.ts
@@ -0,0 +1,14 @@
+import * as util from 'util';
+
+export class Foo {
+    /**
+     * The coolest number.
+     * @type {bigint}
+     */
+    bar: bigint = 1_337n
+}
+
+const o: any = {};
+const f = new Foo;
+console.log(util.inspect(o?.baz ?? f.bar, {colors: true}), process.env.API_KEY);
+throw new Error("typescript trace plz");
