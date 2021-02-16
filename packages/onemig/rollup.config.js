c648643710edd2ee1f818ed94dbaeff9df779b9a
Mark Penner <code@mpen.ca>
2021-02-15 19:54:40 -0800

    No babel config
    --HG--
    branch : v2

--- packages/onemig/rollup.config.js
+++ packages/onemig/rollup.config.js
@@ -1,37 +1,42 @@
-import babel from '@rollup/plugin-babel';
-import nodeResolve from '@rollup/plugin-node-resolve';
-import nodeExternals from 'rollup-plugin-node-externals'
-import * as tsconfig from './tsconfig.json';
-import json from '@rollup/plugin-json';
+// import babel from '@rollup/plugin-babel';
+// import nodeResolve from '@rollup/plugin-node-resolve';
+// import nodeExternals from 'rollup-plugin-node-externals'
+// import * as tsconfig from './tsconfig.json';
+// import json from '@rollup/plugin-json';
+//
+// const extensions = ['.ts'];
+//
+// export default {
+//     input: tsconfig.files,
+//     plugins: [
+//         nodeExternals({
+//             builtins: true,
+//             deps: true,
+//             devDeps: false,
+//             peerDeps: true,
+//             optDeps: true,
+//         }),
+//         json(),
+//         babel({
+//             include: 'src/**',
+//             extensions,
+//             comments: false,
+//             babelHelpers: 'bundled', // https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
+//         }),
+//         nodeResolve({
+//             extensions,
+//             preferBuiltins: true
+//         }),
+//     ],
+//     output: {
+//         banner: '#!/usr/bin/env -S node -max-old-space-size=12000 --enable-source-maps',
+//         dir: 'dist',
+//         format: 'cjs',
+//         sourcemap: true,
+//     },
+// }
 
-const extensions = ['.ts'];
+import tsapp from '@mpen/rollup-preset-tsapp'
 
-export default {
-    input: tsconfig.files,
-    plugins: [
-        nodeExternals({
-            builtins: true,
-            deps: true,
-            devDeps: false,
-            peerDeps: true,
-            optDeps: true,
-        }),
-        json(),
-        babel({
-            include: 'src/**',
-            extensions,
-            comments: false,
-            babelHelpers: 'bundled', // https://github.com/rollup/plugins/tree/master/packages/babel#babelhelpers
-        }),
-        nodeResolve({
-            extensions,
-            preferBuiltins: true
-        }),
-    ],
-    output: {
-        banner: '#!/usr/bin/env -S node -max-old-space-size=12000 --enable-source-maps',
-        dir: 'dist',
-        format: 'cjs',
-        sourcemap: true,
-    },
-}
+export default tsapp()
+
