55d4c2764ab9d5572a0955b5cd25def673b6b30f
Mark Penner <code@mpen.ca>
2022-01-29 13:52:28 -0800

    More features

--- packages/yamake/src/index.ts
+++ packages/yamake/src/index.ts
@@ -8,6 +8,9 @@ import {hrtime} from 'process'
 import * as crypto from 'crypto'
 import {EOL} from 'os'
 import type {WriteStream} from 'node:tty'
+import {SpawnOptionsWithoutStdio} from 'child_process'
+import rimraf from 'rmfr'
+import mkdirp from 'mkdirp'
 
 function objectHash(obj: any): string {
     return crypto.createHash('md5').update(jsonStringify(obj)).digest('base64url')
@@ -21,11 +24,11 @@ function toStringArray(arg: any): string[] {
     return arg.map(x => String(x))
 }
 
-function shellescapeArg(s: string) {
+function escapeShellArg(s: string) {
     if (!/^[A-Za-z0-9=._\/-]+$/.test(s)) {
         s = "'" + s.replace(/'/g, "'\\''") + "'";
-        s = s.replace(/^(?:'')+/g, '') // unduplicate single-quote at the beginning
-            .replace(/\\'''/g, "\\'"); // remove non-escaped single-quote if there are enclosed between 2 escaped
+        // s = s.replace(/^(?:'')+/g, '') // unduplicate single-quote at the beginning
+        //     .replace(/\\'''/g, "\\'"); // remove non-escaped single-quote if there are enclosed between 2 escaped
     }
     return s;
 }
@@ -136,7 +139,42 @@ function formatTime(d: bigint|null) {
     return TIME_FORMATTER.format(nsToMs(d))
 }
 
+interface ParsedArgs {
+    args: string[]
+    flags: Set<string>
+    opts: Record<string,string>
+}
+
+const SHORT_FLAGS = {
+    i: 'interactive'
+}
+
+function parseArgs(args: string[]) {
+    let endOfArgs = false
+    const out: ParsedArgs = {
+        args: [],
+        flags: new Set,
+        opts: {},
+    }
+    for(const arg of args) {
+        if(!endOfArgs && arg === '--') {
+            endOfArgs = true
+        } else if(!endOfArgs && arg.startsWith('-')) {
+            if(arg.startsWith('--')) {
+                out.flags.add(arg.slice(2))
+            } else {
+                out.flags.add(SHORT_FLAGS[arg.slice(1)])
+            }
+        } else {
+            out.args.push(arg)
+        }
+    }
+    return out
+}
+
 async function main(mainArgs: string[]): Promise<number | void> {
+    const parsedArgs = parseArgs(mainArgs)
+    const interactive = parsedArgs.flags.has('interactive')
     const doc = yaml.load(await fs.readFile(MAKE_FILE, 'utf8')) as any
     let cache: any
     try {
@@ -144,10 +182,11 @@ async function main(mainArgs: string[]): Promise<number | void> {
     } catch(_){}
     cache ??= {}
 
-    const ruleName = mainArgs.length >= 1 ? mainArgs[0] : 'default'
+    const ruleName = parsedArgs.args.length >= 1 ? parsedArgs.args[0] : 'default'
 
-    async function resolveRule(ruleName: string): Promise<number> {
-        const info = (...data: any[]) => console.info(Chalk.gray(`[${ruleName}]`),...data)
+    async function resolveRule(ruleName: string, depth:number=0): Promise<number> {
+        // '  '.repeat(depth)+
+        const info = (...data: any[]) => console.info(Chalk.gray(`｢${ruleName}｣`),...data)  // TODO: color each rule differently?
         const rule = doc.rules?.[ruleName]
         if(!rule) {
             console.error(`Rule "${ruleName}" not found`)
@@ -158,9 +197,22 @@ async function main(mainArgs: string[]): Promise<number | void> {
         const auxInput = toStringArray(rule.auxInputs ?? rule.auxInput)
         const output = toStringArray(rule.outputs ?? rule.output)
         const deps = toStringArray(rule.dependencies ?? rule.deps);
+        const odeps = toStringArray(rule.orderedDependencies ?? rule.odeps ?? rule.prerequisites ?? rule.prereqs);
+        const cwd = rule.cwd ?? rule.workingDir ?? rule.workingDirectory ?? process.cwd();
+        const emptyDir = toStringArray(rule.emptyDir)
+        const makeDir = toStringArray(rule.mkdir ?? rule.mkdirp ?? rule.makeDir ?? rule.makeDirectory)  // TODO: allow `true` to create any directories for output files
 
+        if(odeps.length) {
+            for(const dep of odeps) {
+                const exitCode = await resolveRule(dep,depth+1)
+                if(exitCode !== 0) {
+                    info(`Ordered dependency "${dep}" failed with exit code ${exitCode}`)
+                    return exitCode
+                }
+            }
+        }
         if(deps.length) {
-            const exitCodes = await promiseMap(deps, resolveRule);  // TODO: flatten deps into parallelizable tree
+            const exitCodes = await promiseMap(deps, d => resolveRule(d,depth+1));  // TODO: flatten deps into parallelizable tree
             const failedDeps = filterMap(exitCodes, v => v !== 0)
             if(failedDeps.length) {
                 info(`${failedDeps.length} dependencies failed; aborting`, Object.fromEntries(failedDeps))
@@ -185,6 +237,7 @@ async function main(mainArgs: string[]): Promise<number | void> {
         const lastSuccessfulBuildNanos: bigint = lastSuccessfulBuildMs != null ? msToNs(lastSuccessfulBuildMs) : BigInt(Number.MIN_SAFE_INTEGER)
 
         if(doesNotContainNull(outputTimes)) {
+            // FIXME: should dependencies run if nothing's modified? how goes GNU Make do it?
             const auxInputTimes = await getValidMtimesArr(auxInput)
             const allInputTimes = [...inputTimes,...auxInputTimes]
             const inputLastMod = allInputTimes.length ? max(allInputTimes) : null
@@ -200,6 +253,24 @@ async function main(mainArgs: string[]): Promise<number | void> {
         // TODO: check if the input files are the outputs of any other rules and automatically run those build rules too
         // --> what if a file appears in the output of 2 or more different rules...? what does make do?
 
+        if(emptyDir.length) {
+            // FIXME: should this happen *before* evaluating the dependencies?
+            for(const dir of emptyDir) {
+                const fullPath = Path.resolve(cwd,dir)
+                info(`Emptying directory "${fullPath}"`)
+                await rimraf(fullPath)
+                await mkdirp(fullPath)
+            }
+        }
+
+        if(makeDir.length) {
+            for(const dir of makeDir) {
+                const fullPath = Path.resolve(cwd,dir)
+                info(`Creating directory "${fullPath}"`)
+                await mkdirp(fullPath)
+            }
+        }
+
         let cmd: string = rule.command ?? rule.cmd;
         if(cmd == null) {
             info(`No command`)
@@ -209,6 +280,14 @@ async function main(mainArgs: string[]): Promise<number | void> {
         if(Array.isArray(cmd)) {
             [cmd,...cmdArgs] = cmd
         } else {
+            // TODO: https://www.gnu.org/software/make/manual/html_node/Automatic-Variables.html
+            // TODO: support $1, $2, ... command-line args
+            if(input.length) {
+                cmd = cmd.replace(/(?<!\\)\$</g, escapeShellArg(input[0]))
+            }
+            if(output.length) {
+                cmd = cmd.replace(/(?<!\\)\$@/g, output.map(o => escapeShellArg(o)).join(' '))
+            }
             cmdArgs = toStringArray(rule.arguments ?? rule.args)
         }
 
@@ -232,7 +311,7 @@ async function main(mainArgs: string[]): Promise<number | void> {
 
 
         const start = hrtime.bigint()
-        const code = await spawn(ruleName, cmd, cmdArgs, {suppressOutput})
+        const code = await spawn(ruleName, cmd, cmdArgs, {suppressOutput,interactive,cwd})
         const elapsed = nsToMs(hrtime.bigint() - start)
         info(`Exited with code ${Chalk[code === 0 ? 'green' : 'red'](code)} in ${Chalk.blue(elapsed)}ms`)
         if(code === 0) {
@@ -296,11 +375,19 @@ function pipe2console(prefix: string, stream: WriteStream) {
 
 interface SpawnOptions {
     suppressOutput?: boolean
+    interactive?: boolean
+    cwd?: string
 }
 
 function spawn(prefix: string, cmd: string, args: string[], opts: SpawnOptions): Promise<number> {
     return new Promise((resolve,reject) => {
-        const proc = childProc.spawn(cmd,args,{stdio: opts.suppressOutput ? 'ignore' : ['ignore','pipe','pipe'],shell:true})  // FIXME: I don't think `cmd` is escaped when shell:true
+        const spawnOpts: SpawnOptionsWithoutStdio = {
+            stdio: opts.suppressOutput ? 'ignore' : [opts.interactive ? 'inherit' : 'ignore','pipe','pipe'],
+            shell: true,
+            cwd: opts.cwd,
+        }
+
+        const proc = childProc.spawn(cmd,args,spawnOpts)  // FIXME: I don't think `cmd` is escaped when shell:true
         if(!opts.suppressOutput) {
             proc.stdout!.on('data', pipe2console(prefix, process.stdout))
             proc.stderr!.on('data', pipe2console(prefix, process.stderr))
