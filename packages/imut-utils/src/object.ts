62e1b53b4bec264e604b3d7eff3bffea1e1ec297
Mark Penner <code@mpen.ca>
2023-01-05 20:35:08 -0800

    Publish v0.1.18

--- packages/imut-utils/src/object.ts
+++ packages/imut-utils/src/object.ts
@@ -1,15 +1,14 @@
-import type {ValueOf} from './types'
 import {Resolvable, resolveValue} from './resolvable'
 
 
 export function fpShallowMerge<T>(...objects: {
-    [P in keyof T]?: Resolvable<ValueOf<T>,[ValueOf<T>,keyof T]>;
-}[]): (obj:T)=>T&{} {
+    [K in keyof T]?: Resolvable<T[K], [T[K], K]>;
+}[]): (obj: T) => T & {} {
     return (obj: T) => {
         const ret = Object.assign(Object.create(null), obj)
         for(const o of objects) {
             for(const k of Object.keys(o) as (keyof T)[]) {
-                ret[k] = resolveValue(o[k],ret[k],k)
+                ret[k] = resolveValue(o[k], ret[k], k)
             }
         }
         return ret
