22201ceebceb2dd0d9c0d453fb8e9704555eefec
Mark Penner <mark@mpen.ca>
2023-01-20 19:03:21 -0800

    Publish v0.1.33

--- packages/imut-utils/rollup.config.js
+++ packages/imut-utils/rollup.config.js
@@ -3,11 +3,18 @@ import terser from '@rollup/plugin-terser';
 
 export default {
     input: 'src/index.ts',
-    output: {
-        dir: 'dist',
-        format: 'esm',
-        entryFileNames: '[name].mjs',
-    },
+    output: [
+        {
+            dir: 'dist',
+            format: 'esm',
+            entryFileNames: '[name].mjs',
+        },
+        {
+            dir: 'dist',
+            format: 'cjs',
+            entryFileNames: '[name].cjs',
+        },
+    ],
     plugins: [
         typescript(),
         terser({
