69404b14d611d4e9da045b502a4fef64fda0a719
Mark Penner <mark@mpen.ca>
2023-02-13 20:42:02 -0800

    Publish v0.1.38

--- packages/imut-utils/src/number.test.ts
+++ packages/imut-utils/src/number.test.ts
@@ -1,4 +1,4 @@
-import {add, fpAdd, fpDiv, fpMult} from './number'
+import {add, div, fpAdd, fpDiv, fpMult, fpSub, mult, sub} from './number'
 
 
 test(add.name, () => {
@@ -13,9 +13,31 @@ test(fpAdd.name, () => {
     expect(fpAdd(3)(undefined)).toBe(3)
 })
 
+test(sub.name, () => {
+    expect(sub(1,2)).toBe(-1)
+    expect(sub(null,2)).toBe(-2)
+    expect(sub(undefined,3)).toBe(-3)
+})
+
+test(fpSub.name, () => {
+    expect(fpSub(2)(1)).toBe(-1)
+    expect(fpSub(2)(null)).toBe(-2)
+    expect(fpSub(3)(undefined)).toBe(-3)
+})
+
+test(mult.name, () => {
+    expect(mult(2,6)).toBe(12)
+    expect(mult(undefined,2)).toBe(0)
+})
+
 test(fpMult.name, () => {
     expect(fpMult(2)(6)).toBe(12)
     expect(fpMult(2)(undefined)).toBe(0)
+})
+
+test(div.name, () => {
+    expect(div(6,2)).toBe(3)
+    expect(div(null,2)).toBe(0)
 })
 
 test(fpDiv.name, () => {
--- packages/imut-utils/src/number.ts
+++ packages/imut-utils/src/number.ts
@@ -1,24 +1,43 @@
 import {nil} from './types'
 
-
-export function add(prev: number|nil, value:number) {
-    return (prev??0) + value
+/**
+ * Adds 2 numbers. `prev` defaults to 0 if undefined.
+ */
+export function add(a: number|nil, b:number) {
+    return (a??0) + b
 }
 
 export function fpAdd(value: number) {
     return (prev: number|nil) => (prev??0) + value
 }
 
-export function mult(prev: number|nil, value:number) {
-    return (prev??0) * value
+/**
+ * Subtracts 2 numbers. `a` defaults to 0 if undefined.
+ */
+export function sub(a: number|nil, b:number) {
+    return (a??0) - b
+}
+
+export function fpSub(value: number) {
+    return (prev: number|nil) => (prev??0) - value
+}
+
+/**
+ * Multiplies 2 numbers. `a` defaults to 0 if undefined.
+ */
+export function mult(a: number|nil, b:number) {
+    return (a??0) * b
 }
 
 export function fpMult(value: number) {
     return (prev: number|nil) => (prev??0) * value
 }
 
-export function div(prev: number|nil, value:number) {
-    return (prev??0) / value
+/**
+ * Divides 2 numbers. `a` defaults to 0 if undefined.
+ */
+export function div(a: number|nil, b:number) {
+    return (a??0) / b
 }
 
 export function fpDiv(value: number) {
