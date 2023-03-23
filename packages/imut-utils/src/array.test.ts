93ef5ec89f4fb923ae093588ad10a7bf522da795
Mark Penner <code@mpen.ca>
2023-03-22 23:30:17 -0700

    Publish v0.1.42

--- packages/imut-utils/src/array.test.ts
+++ packages/imut-utils/src/array.test.ts
@@ -10,24 +10,24 @@ import {
     fpArrayInsert,
     arrayInsertSorted,
     fpArrayInsertSorted,
-    fpArrayPop, fpArrayDeleteValue
+    fpArrayPop, fpArrayDeleteValue, fpArrayFindAndReplace, fpArraySplice
 } from './array'
 
 
 test(fpArrayPush.name, () => {
-    const arr = [3,1,2,7,4,6,5]
-    const out = fpArrayPush(1,5,3)(arr)
-    expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-    expect(out).toStrictEqual([3,1,2,7,4,6,5,1,5,3])
+    const arr = [3, 1, 2, 7, 4, 6, 5]
+    const out = fpArrayPush(1, 5, 3)(arr)
+    expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+    expect(out).toStrictEqual([3, 1, 2, 7, 4, 6, 5, 1, 5, 3])
 })
 
 describe(fpArrayPop.name, () => {
     it('takes 0 args', () => {
-        expect(fpArrayPop()([1,2,3])).toStrictEqual([1,2])
+        expect(fpArrayPop()([1, 2, 3])).toStrictEqual([1, 2])
     })
     it('throws on bad input', () => {
-        expect(() => fpArrayPop(-1)([1,2,3])).toThrow()
-        expect(() => fpArrayPop(3.14)([1,2,3])).toThrow()
+        expect(() => fpArrayPop(-1)([1, 2, 3])).toThrow()
+        expect(() => fpArrayPop(3.14)([1, 2, 3])).toThrow()
     })
     it('pops multiple', () => {
         expect(fpArrayPop(0)([1, 2, 3])).toStrictEqual([1, 2, 3])
@@ -45,45 +45,45 @@ describe(fpArrayPop.name, () => {
 
 
 test(fpArrayUnshift.name, () => {
-    const arr = [3,1,2,7,4,6,5]
-    const out = fpArrayUnshift(1,5,3)(arr)
-    expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-    expect(out).toStrictEqual([1,5,3,3,1,2,7,4,6,5])
+    const arr = [3, 1, 2, 7, 4, 6, 5]
+    const out = fpArrayUnshift(1, 5, 3)(arr)
+    expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+    expect(out).toStrictEqual([1, 5, 3, 3, 1, 2, 7, 4, 6, 5])
 })
 
 test(fpArrayInsert.name, () => {
-    expect(fpArrayInsert(0,99)([1,3,5])).toStrictEqual([99,1,3,5])
-    expect(fpArrayInsert(1,99)([1,3,5])).toStrictEqual([1,99,3,5])
-    expect(fpArrayInsert(1,99)([1,3,5])).toStrictEqual([1,99,3,5])
-    expect(fpArrayInsert(9,99)([1,3,5])).toStrictEqual([1,3,5,99])
-    expect(fpArrayInsert(2,77,88,99)([1,3,5])).toStrictEqual([1,3,77,88,99,5])
-    expect(fpArrayInsert(2)([1,3,5])).toStrictEqual([1,3,5])
+    expect(fpArrayInsert(0, 99)([1, 3, 5])).toStrictEqual([99, 1, 3, 5])
+    expect(fpArrayInsert(1, 99)([1, 3, 5])).toStrictEqual([1, 99, 3, 5])
+    expect(fpArrayInsert(1, 99)([1, 3, 5])).toStrictEqual([1, 99, 3, 5])
+    expect(fpArrayInsert(9, 99)([1, 3, 5])).toStrictEqual([1, 3, 5, 99])
+    expect(fpArrayInsert(2, 77, 88, 99)([1, 3, 5])).toStrictEqual([1, 3, 77, 88, 99, 5])
+    expect(fpArrayInsert(2)([1, 3, 5])).toStrictEqual([1, 3, 5])
 })
 
 test(fpArrayInsertSorted.name, () => {
-    expect(fpArrayInsertSorted(6,2,4)([1,3,5,7])).toStrictEqual([1,2,3,4,5,6,7])
-    expect(fpArrayInsertSorted(9,-9)([1,3,5,7])).toStrictEqual([-9,1,3,5,7,9])
-    expect(fpArrayInsertSorted(3,5)([1,3,5,7])).toStrictEqual([1,3,3,5,5,7])
+    expect(fpArrayInsertSorted(6, 2, 4)([1, 3, 5, 7])).toStrictEqual([1, 2, 3, 4, 5, 6, 7])
+    expect(fpArrayInsertSorted(9, -9)([1, 3, 5, 7])).toStrictEqual([-9, 1, 3, 5, 7, 9])
+    expect(fpArrayInsertSorted(3, 5)([1, 3, 5, 7])).toStrictEqual([1, 3, 3, 5, 5, 7])
 })
 
 
 test(fpArrayDeleteIndex.name, () => {
-    const arr = [3,1,2,7,4,6,5]
-    const out = fpArrayDeleteIndex(1,5,3)(arr)
-    expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-    expect(out).toStrictEqual([3,2,4,5])
+    const arr = [3, 1, 2, 7, 4, 6, 5]
+    const out = fpArrayDeleteIndex(1, 5, 3)(arr)
+    expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+    expect(out).toStrictEqual([3, 2, 4, 5])
 })
 
 describe(fpArraySelect.name, () => {
     it('basic', () => {
-        const arr = [3,1,2,7,4,6,5]
+        const arr = [3, 1, 2, 7, 4, 6, 5]
         const out = fpArraySelect<number>((v, i) => v > 5 || i === 0)(arr)
-        expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-        expect(out).toStrictEqual([3,7,6])
+        expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+        expect(out).toStrictEqual([3, 7, 6])
     })
     it('more', () => {
-        expect(fpArraySelect<number>(n => n % 2 === 0)([1, 2, 3, 4, 5, 6])).toEqual([2,4,6])
-        expect(fpArraySelect<number>(n => n % 2 === 0, 2)([1, 2, 3, 4, 5, 6])).toEqual([2,4])
+        expect(fpArraySelect<number>(n => n % 2 === 0)([1, 2, 3, 4, 5, 6])).toEqual([2, 4, 6])
+        expect(fpArraySelect<number>(n => n % 2 === 0, 2)([1, 2, 3, 4, 5, 6])).toEqual([2, 4])
         expect(fpArraySelect<number>(n => n % 2 === 0, 0)([1, 2, 3, 4, 5, 6])).toEqual([])
     })
 })
@@ -103,7 +103,7 @@ describe(fpArrayReject.name, () => {
     })
 })
 
-describe(fpArrayDeleteValue.name, () => {
+test(fpArrayDeleteValue.name, () => {
     expect(fpArrayDeleteValue<number>(2, true)([1, 2, 1, 2])).toEqual([1, 1])
     expect(fpArrayDeleteValue<number>(2, false)([1, 2, 1, 2])).toEqual([1, 1])
     expect(fpArrayDeleteValue<number | string>('2', false)([1, 2, 1, 2])).toEqual([1, 1])
@@ -114,27 +114,27 @@ describe(fpArrayDeleteValue.name, () => {
 
 describe(fpArrayDeleteOneValue.name, () => {
     it('deletes one value', () => {
-        const arr = [1,9,2,9,3]
-        const out = fpArrayDeleteOneValue<number>(9,false)(arr)
-        expect(arr).toStrictEqual([1,9,2,9,3])
-        expect(out).toStrictEqual([1,2,9,3])
+        const arr = [1, 9, 2, 9, 3]
+        const out = fpArrayDeleteOneValue<number>(9, false)(arr)
+        expect(arr).toStrictEqual([1, 9, 2, 9, 3])
+        expect(out).toStrictEqual([1, 2, 9, 3])
     })
     it('performs loose comparison', () => {
-        const arr = [1,'2',2,'3']
-        const out = fpArrayDeleteOneValue<string|number>(2,false)(arr)
-        expect(arr).toStrictEqual([1,'2',2,'3'])
-        expect(out).toStrictEqual([1,2,'3'])
+        const arr = [1, '2', 2, '3']
+        const out = fpArrayDeleteOneValue<string | number>(2, false)(arr)
+        expect(arr).toStrictEqual([1, '2', 2, '3'])
+        expect(out).toStrictEqual([1, 2, '3'])
     })
     it('performs strict comparison', () => {
-        const arr = [1,'2',2,'3']
-        const out = fpArrayDeleteOneValue<string|number>(2,true)(arr)
-        expect(arr).toStrictEqual([1,'2',2,'3'])
-        expect(out).toStrictEqual([1,'2','3'])
+        const arr = [1, '2', 2, '3']
+        const out = fpArrayDeleteOneValue<string | number>(2, true)(arr)
+        expect(arr).toStrictEqual([1, '2', 2, '3'])
+        expect(out).toStrictEqual([1, '2', '3'])
     })
     it('no match', () => {
-        const arr = [1,9,2,9,3]
-        const out = fpArrayDeleteOneValue<number>(10,false)(arr)
-        expect(arr).toStrictEqual([1,9,2,9,3])
+        const arr = [1, 9, 2, 9, 3]
+        const out = fpArrayDeleteOneValue<number>(10, false)(arr)
+        expect(arr).toStrictEqual([1, 9, 2, 9, 3])
         expect(out).toBe(arr)
     })
 })
@@ -142,36 +142,50 @@ describe(fpArrayDeleteOneValue.name, () => {
 
 describe(arraySortNumbers.name, () => {
     it('sorts ascending', () => {
-        const arr = [3,1,2,7,4,6,5]
+        const arr = [3, 1, 2, 7, 4, 6, 5]
         const out = arraySortNumbers(arr)
-        expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-        expect(out).toStrictEqual([1,2,3,4,5,6,7])
+        expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+        expect(out).toStrictEqual([1, 2, 3, 4, 5, 6, 7])
     })
     it('sorts descending', () => {
-        const arr = [3,1,2,7,4,6,5]
+        const arr = [3, 1, 2, 7, 4, 6, 5]
         const out = arraySortNumbers(arr, false)
-        expect(arr).toStrictEqual([3,1,2,7,4,6,5])
-        expect(out).toStrictEqual([7,6,5,4,3,2,1])
+        expect(arr).toStrictEqual([3, 1, 2, 7, 4, 6, 5])
+        expect(out).toStrictEqual([7, 6, 5, 4, 3, 2, 1])
     })
 })
 
 describe(arraySortStrings.name, () => {
     it('sorts alphabetically', () => {
-        const arr = ['alpha','charlie','beta']
+        const arr = ['alpha', 'charlie', 'beta']
         const out = arraySortStrings(arr)
-        expect(arr).toStrictEqual(['alpha','charlie','beta'])
-        expect(out).toStrictEqual(['alpha','beta','charlie'])
+        expect(arr).toStrictEqual(['alpha', 'charlie', 'beta'])
+        expect(out).toStrictEqual(['alpha', 'beta', 'charlie'])
     })
     it('sorts case-insensitive', () => {
-        expect(arraySortStrings(['AA','ab','aa']))
-            .toStrictEqual(['AA','aa','ab'])
+        expect(arraySortStrings(['AA', 'ab', 'aa']))
+            .toStrictEqual(['AA', 'aa', 'ab'])
     })
     it('sorts numbers', () => {
-        expect(arraySortStrings(['a2','a10','a1']))
-            .toStrictEqual(['a1','a2','a10'])
+        expect(arraySortStrings(['a2', 'a10', 'a1']))
+            .toStrictEqual(['a1', 'a2', 'a10'])
     })
     it('sorts descending', () => {
-        expect(arraySortStrings(['c','ab','b','aa'],{ascending:false}))
-            .toStrictEqual(['c','b','ab','aa'])
+        expect(arraySortStrings(['c', 'ab', 'b', 'aa'], {ascending: false}))
+            .toStrictEqual(['c', 'b', 'ab', 'aa'])
     })
+})
+
+test(fpArrayFindAndReplace.name, () => {
+    expect(fpArrayFindAndReplace(x => x === 5, 3)([1, 2, 5, 4])).toEqual([1, 2, 3, 4])
+    expect(fpArrayFindAndReplace(x => x === 6, 3)([1, 2, 5, 4])).toEqual([1, 2, 5, 4])
+})
+
+test(fpArraySplice.name, () => {
+    expect(fpArraySplice(2)([1, 2, 3, 4])).toEqual([1, 2, 4])
+    expect(fpArraySplice(2, 2)([1, 2, 3, 4])).toEqual([1, 2])
+    expect(fpArraySplice(2, 1, 7, 8, 9)([1, 2, 3, 4])).toEqual([1, 2, 7, 8, 9, 4])
+    expect(fpArraySplice(1, 0, 9)([1, 2, 3, 4])).toEqual([1, 9, 2, 3, 4])
+    expect(fpArraySplice(0, 0, 9)([1, 2, 3, 4])).toEqual([9, 1, 2, 3, 4])
+    expect(fpArraySplice(9, 0, 9)([1, 2, 3, 4])).toEqual([1, 2, 3, 4, 9])
 })
--- packages/imut-utils/src/array.ts
+++ packages/imut-utils/src/array.ts
@@ -1,42 +1,44 @@
 import type {nil} from './types'
-import {binarySearch,assert} from './extra'
+import {binarySearch, assert} from './extra'
 
 /**
  * Appends elements onto the end of the array.
  */
-export function arrayPush<T>(array: T[]|nil, ...values: T[]): T[] {
-    return [...array??[], ...values]
+export function arrayPush<T>(array: T[] | nil, ...values: T[]): T[] {
+    return [...array ?? [], ...values]
 }
 
 export function fpArrayPush<T>(...values: T[]) {
-    return (a: T[]|nil) => arrayPush(a, ...values)
+    return (a: T[] | nil) => arrayPush(a, ...values)
 }
 
 
 /**
  * Pops an element off the end of the array.
  */
-export function arrayPop<T>(array: T[]|nil,howMany=1): T[] {
+export function arrayPop<T>(array: T[] | nil, howMany = 1): T[] {
     assert(Number.isInteger(howMany))
     assert(howMany >= 0)
     if(!array) return []
     if(howMany === 0) return array
-    return array.slice(0,-howMany)
+    return array.slice(0, -howMany)
 }
-export function fpArrayPop<T>(howMany=1) {
-    return (a: T[]|nil) => arrayPop(a, howMany)
+
+export function fpArrayPop<T>(howMany = 1) {
+    return (a: T[] | nil) => arrayPop(a, howMany)
 }
 
 /**
  * Insert one or more elements into an array at the given position.
  */
-export function arrayInsert<T>(array: T[]|nil, index:number, ...values: T[]): T[] {
+export function arrayInsert<T>(array: T[] | nil, index: number, ...values: T[]): T[] {
     const copy = array ? [...array] : []
-    copy.splice(index,0,...values)
+    copy.splice(index, 0, ...values)
     return copy
 }
-export function fpArrayInsert<T>(index:number, ...values: T[]) {
-    return (a: T[]|nil) => arrayInsert(a, index, ...values)
+
+export function fpArrayInsert<T>(index: number, ...values: T[]) {
+    return (a: T[] | nil) => arrayInsert(a, index, ...values)
 }
 
 /**
@@ -45,34 +47,35 @@ export function fpArrayInsert<T>(index:number, ...values: T[]) {
  * @param array Array of numbers. Must be sorted in ascending order.
  * @param values Values to insert. These needn't be sorted; each one will be searched for individually.
  */
-export function arrayInsertSorted(array: number[]|nil, ...values: number[]): number[] {
+export function arrayInsertSorted(array: number[] | nil, ...values: number[]): number[] {
     const copy = array ? [...array] : []
     for(const v of values) {
-        const index = binarySearch(copy,v)
-        copy.splice(index < 0 ? ~index : index,0,v)
+        const index = binarySearch(copy, v)
+        copy.splice(index < 0 ? ~index : index, 0, v)
     }
     return copy
 }
+
 export function fpArrayInsertSorted<T>(...values: number[]) {
-    return (a: number[]|nil) => arrayInsertSorted(a, ...values)
+    return (a: number[] | nil) => arrayInsertSorted(a, ...values)
 }
 
 
 /**
  * Prepends elements onto the end of the array.
  */
-export function arrayUnshift<T>(array: T[]|nil, ...values: T[]): T[] {
-    return [...values, ...array??[]]
+export function arrayUnshift<T>(array: T[] | nil, ...values: T[]): T[] {
+    return [...values, ...array ?? []]
 }
 
 export function fpArrayUnshift<T>(...values: T[]) {
-    return (a: T[]|nil) => arrayUnshift(a, ...values)
+    return (a: T[] | nil) => arrayUnshift(a, ...values)
 }
 
 /**
  * Deletes elements from an array by index.
  */
-export function arrayDeleteIndex<T>(array: T[]|nil, ...indices: number[]): T[] {
+export function arrayDeleteIndex<T>(array: T[] | nil, ...indices: number[]): T[] {
     if(!array?.length) return []
     indices.sort((a, b) => b - a)
     const ret = [...array]
@@ -83,7 +86,7 @@ export function arrayDeleteIndex<T>(array: T[]|nil, ...indices: number[]): T[] {
 }
 
 export function fpArrayDeleteIndex<T>(...indices: number[]) {
-    return (a: T[]|nil) => arrayDeleteIndex(a, ...indices)
+    return (a: T[] | nil) => arrayDeleteIndex(a, ...indices)
 }
 
 function looseEq(a: any, b: any) {
@@ -102,7 +105,7 @@ function fpStrictEq(value: any) {
  * Deletes up to one element from an array, searching by value.
  * @deprecated
  */
-export function arrayDeleteOneValue<T>(array: T[]|nil, value: T, strict: boolean): T[] {
+export function arrayDeleteOneValue<T>(array: T[] | nil, value: T, strict: boolean): T[] {
     if(!array?.length) return []
     const idx = array.findIndex((strict ? fpStrictEq : fpLooseEq)(value))
     return idx < 0 ? array : arrayDeleteIndex(array, idx)
@@ -112,33 +115,33 @@ export function arrayDeleteOneValue<T>(array: T[]|nil, value: T, strict: boolean
  * @deprecated
  */
 export function fpArrayDeleteOneValue<T>(value: T, strict: boolean) {
-    return (a: T[]|nil) => arrayDeleteOneValue(a, value, strict)
+    return (a: T[] | nil) => arrayDeleteOneValue(a, value, strict)
 }
 
 /**
  * Deletes up to one element from an array, searching by value.
  */
-export function arrayDeleteValue<T>(array: T[]|nil, value: T, strict: boolean, limit?: number): T[] {
-    return arrayReject(array, strict ? x => Object.is(x,value) : x => x == value, limit)
+export function arrayDeleteValue<T>(array: T[] | nil, value: T, strict: boolean, limit?: number): T[] {
+    return arrayReject(array, strict ? x => Object.is(x, value) : x => x == value, limit)
 }
 
 export function fpArrayDeleteValue<T>(value: T, strict: boolean, limit?: number) {
-    return (a: T[]|nil) => arrayDeleteValue(a, value, strict, limit)
+    return (a: T[] | nil) => arrayDeleteValue(a, value, strict, limit)
 }
 
 /**
  * Filters the array to elements that pass the predicate.
  * If limit is provided, returned array will be at most that length.
  */
-export function arraySelect<T>(array: T[]|nil, predicate: (v: T, i: number) => boolean, limit?: number): T[] {
+export function arraySelect<T>(array: T[] | nil, predicate: (v: T, i: number) => boolean, limit?: number): T[] {
     if(!array?.length) return []
     if(limit == null) {
         return array.filter((v, i) => predicate(v, i))
     }
     if(limit <= 0) return []
     let selected: T[] = []
-    for(const [i,v] of array.entries()) {
-        if(predicate(v,i)) {
+    for(const [i, v] of array.entries()) {
+        if(predicate(v, i)) {
             selected.push(v)
             if(selected.length >= limit) break
         }
@@ -147,14 +150,14 @@ export function arraySelect<T>(array: T[]|nil, predicate: (v: T, i: number) => b
 }
 
 export function fpArraySelect<T>(predicate: (v: T, i: number) => boolean, limit?: number) {
-    return (a: T[]|nil) => arraySelect(a, predicate, limit)
+    return (a: T[] | nil) => arraySelect(a, predicate, limit)
 }
 
 /**
  * Removes elements that do *not* pass the predicate.
  * If limit is provided, at most that many elements will be deleted. i.e. new length will be >= array.length - limit.
  */
-export function arrayReject<T>(array: T[]|nil, predicate: (v: T, i: number) => boolean, limit?: number): T[] {
+export function arrayReject<T>(array: T[] | nil, predicate: (v: T, i: number) => boolean, limit?: number): T[] {
     if(!array?.length) return []
     if(limit == null) {
         return array.filter((v, i) => !predicate(v, i))
@@ -162,8 +165,8 @@ export function arrayReject<T>(array: T[]|nil, predicate: (v: T, i: number) => b
     if(limit <= 0) return array
     let selected: T[] = []
     let rejected = 0
-    for(const [i,v] of array.entries()) {
-        if(rejected < limit && predicate(v,i)) {
+    for(const [i, v] of array.entries()) {
+        if(rejected < limit && predicate(v, i)) {
             ++rejected
         } else {
             selected.push(v)
@@ -176,12 +179,12 @@ export function fpArrayReject<T>(predicate: (v: T, i: number) => boolean, limit?
     return (a: T[]) => arrayReject(a, predicate, limit)
 }
 
-export function arraySort<T>(array: T[]|nil, compareFn: (a: T, b: T) => number) {
+export function arraySort<T>(array: T[] | nil, compareFn: (a: T, b: T) => number) {
     if(!array?.length) return []
     return [...array].sort(compareFn)
 }
 
-export function arraySortNumbers(array: number[]|nil, ascending=true) {
+export function arraySortNumbers(array: number[] | nil, ascending = true) {
     if(!array?.length) return []
     return arraySort(array, ascending ? (a, b) => a - b : (a, b) => b - a)
 }
@@ -197,7 +200,7 @@ interface CollatorOptions extends Intl.CollatorOptions {
     ascending?: boolean
 }
 
-export function arraySortStrings(array: string[]|nil, options: CollatorOptions = {
+export function arraySortStrings(array: string[] | nil, options: CollatorOptions = {
     sensitivity: 'base',
     numeric: true,
     usage: 'sort',
@@ -206,7 +209,38 @@ export function arraySortStrings(array: string[]|nil, options: CollatorOptions =
     if(!array?.length) return []
     const {locales, ascending, ...opts} = options
     const collator = new Intl.Collator(locales, opts)
-    const compare = ascending ? collator.compare : (a:string,b:string) => -collator.compare(a,b)
+    const compare = ascending ? collator.compare : (a: string, b: string) => -collator.compare(a, b)
     return arraySort(array, compare)
 }
 
+/**
+ * Replaces `count` elements starting at the given `index` with `replaceWith` elements.
+ *
+ * @param array Array to remove value from
+ * @param index Index to remove
+ * @param count
+ * @param replaceWith
+ * @returns Array with `value` removed
+ */
+export function arraySplice<T>(array: T[]|nil, index: number, count = 1, ...replaceWith: T[]): T[] {
+    array ??= []
+    return [
+        ...array.slice(0, index),
+        ...replaceWith,
+        ...array.slice(index + count),
+    ]
+}
+
+export function fpArraySplice<T>(index: number, count = 1, ...replaceWith: T[]) {
+    return (a: T[]|nil) => arraySplice(a, index, count, ...replaceWith)
+}
+
+export function arrayFindAndReplace<T>(array: T[], predicate: (v: T, i: number) => boolean, replaceWith: T): T[] {
+    const idx = array.findIndex(predicate)
+    if(idx < 0) return array
+    return arraySplice(array, idx, 1, replaceWith)
+}
+
+export function fpArrayFindAndReplace<T>(predicate: (v: T, i: number) => boolean, replaceWith: T) {
+    return (a: T[]|nil) => arrayFindAndReplace(a ?? [], predicate, replaceWith)
+}
