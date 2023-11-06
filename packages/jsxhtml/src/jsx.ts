99e43984732b310ead25c85e434fb639a44a7095
Mark Penner <code@mpen.ca>
2023-11-05 23:08:38 -0800

    Finally fixed the "children" issue

--- packages/jsxhtml/src/jsx.ts
+++ packages/jsxhtml/src/jsx.ts
@@ -6,5 +6,12 @@ declare namespace JSX {
     type IntrinsicElements = {
         [_ in import('./htmlspec/IntrinsicElements').HtmlElements]: import('./types').CommonProps
     }
+
     type Element = import('./jsx-node').JsxNode
+
+    // Fixes "TS2741: Property  children  is missing in type  {}  but required in type  StringChildren "
+    // https://www.typescriptlang.org/docs/handbook/jsx.html#children-type-checking
+    interface ElementChildrenAttribute {
+        children: unknown  // specify children name to use
+    }
 }
