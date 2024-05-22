242d81402c286ee909b19b949e0d79c76a4ec9b3
Mark Penner <code@mpen.ca>
2024-05-21 21:30:47 -0700

    V2 WIP

---
+++ packages/react-basic-inputs/v2/vite.config.ts
@@ -0,0 +1,30 @@
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import dts from "vite-plugin-dts";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+            rollupTypes: true,
+        }),
+    ],
+    clearScreen: false,
+
+    // https://vitejs.dev/guide/build#library-mode
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/bundle.ts'),
+            name: '@mpen/react-basic-inputs',
+            fileName: 'react-basic-inputs',
+        },
+        rollupOptions: {
+            external: ['react','react-dom'],
+
+        }
+    },
+})
