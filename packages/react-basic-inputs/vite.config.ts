54e3a3e526120d43211399df351f239d51da3147
Mark Penner <code@mpen.ca>
2024-05-21 22:32:35 -0700

    Probably good enough

--- packages/react-basic-inputs/vite.config.ts
+++ packages/react-basic-inputs/vite.config.ts
@@ -1,12 +1,13 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'node:path'
-import dts from "vite-plugin-dts";
+import {resolve} from 'node:path'
+import dts from "vite-plugin-dts"
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    root: 'src',
+    // root: 'src',
+    publicDir: 'public',
     plugins: [
         react(),
         dts({
@@ -15,21 +16,31 @@ export default defineConfig({
         }),
     ],
     clearScreen: false,
+    // envDir: __dirname,
 
     // https://vitejs.dev/guide/build#library-mode
     build: {
+        // outDir: resolve(__dirname, 'dist'),
+        // emptyOutDir: true,
+        minify: false,
         lib: {
             entry: resolve(__dirname, 'src/bundle.ts'),
             name: '@mpen/react-basic-inputs',
             fileName: 'react-basic-inputs',
         },
         rollupOptions: {
-            external: ['react','react-dom'],
+            external: [
+                'react',
+                'react-dom',
+                'react/jsx-runtime',
+            ],
             output: {
                 // Provide global variables to use in the UMD build
                 // for externalized deps
                 globals: {
-                    react: 'React',
+                    'react': 'React',
+                    'react-dom': 'ReactDOM',
+                    'react/jsx-runtime': 'jsxRuntime',
                 },
             },
         }
