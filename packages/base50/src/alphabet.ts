f37dc86ed6de42285ef8c4949a40753392d768f8
Mark Penner <code@mpen.ca>
2024-08-12 01:56:36 -0700

    Kind of maybe working in one direction?!?!

--- packages/base50/src/alphabet.ts
+++ packages/base50/src/alphabet.ts
@@ -1,28 +1,26 @@
-import assert from 'assert'
+import assert from 'node:assert'
 
 
-const ZERO = '0'.codePointAt(0)
-const a = 'a'.codePointAt(0)
-const A = 'A'.codePointAt(0)
+// const ZERO = '0'.codePointAt(0)
+// const a = 'a'.codePointAt(0)
+// const A = 'A'.codePointAt(0)
 
 
-export const alphabet = '0123456789bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
+export const ALPHABET = '0123456789bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
+export const BASE = ALPHABET.length
 
-export const BASE = alphabet.length
+const REVERSE = new Map(Array.from(ALPHABET, (v, i) => [v, i]))
 
-const reverse = new Map<string,number>
-for(let i=0; i<alphabet.length; ++i) {
-    reverse.set(alphabet[i], i)
-}
+console.log(REVERSE)
 
 
 export function charToNum(ch: string): number {
     assert(ch?.length === 1)
-    const value = reverse.get(ch)
+    const value = REVERSE.get(ch)
     assert(value != null)
     return value
 }
 
 export function numToChar(num: number): string {
-    return alphabet[num]
+    return ALPHABET[num]
 }
--- packages/base50/src/buffer.ts
+++ packages/base50/src/buffer.ts
@@ -1,30 +1,143 @@
 import {divqr} from './numbers'
-import {alphabet, BASE, numToChar} from './alphabet'
-
+import {ALPHABET, BASE, charToNum, numToChar} from './alphabet'
+import assert from 'node:assert'
 
 type BufferType = Buffer | Uint8Array | number[]
 
-export function bufToBase50(buf: BufferType): string {
-    let accum = 0
-    let carry = 0
-    let out = ''
-    for(let chunk of chunk6bits(buf)) {
-        // if(chunk >= 50) {
-        //     carry = chunk - 50
-        //     chunk -= 50
-        // } else {
-        //     carry = 0
-        // }
-        // accum += 14
-        out  += numToChar(chunk)
+const last6 = 0b00111111
+
+const first2 = 0b11000000
+const all1s = 0xFFFFFFFF
+// const mask4 = 0b11110000
+// const mask2 = 0b11000000
+
+function toBinary(num: number): string {
+    let binaryStr = num.toString(2);
+    const padding = 8 - (binaryStr.length % 8);
+
+    if (padding !== 8) {
+        binaryStr = '0'.repeat(padding) + binaryStr;
     }
+
+    // Insert spaces between each octet
+    return binaryStr.match(/.{8}/g)?.join(' ') ?? binaryStr;
+}
+
+function rightAlignedMask(N: number) {
+    return (1 << N) - 1;
+}
+
+export function bufToBase50(buf: Iterable<number>): string {
+    let bitAccum = 0;
+    let bits = 0;
+    let out = ''
+    let valueAccum = 0;
+
+    for (let byte of buf) {
+        bitAccum = (bitAccum << 8) | byte;
+        bits += 8;
+
+        while (bits >= 6) {
+            // console.log(bits)
+            bits -= 6
+            const value = ((bitAccum>>bits) & last6)+valueAccum
+            bitAccum &= rightAlignedMask(bits);
+            const [q,r] = divqr(value, BASE);
+            // console.log(bits,bitAccum,value,q,r)
+            out += numToChar(r);
+            valueAccum += q * BASE;
+            // console.log('B',bitAccum)
+        }
+    }
+
+    if(bits > 0) {
+        // bits -= 6
+        // console.log(bitAccum)
+        let value = (bitAccum & rightAlignedMask(bits))+valueAccum
+        while(value > 0) {
+            const [q, r] = divqr(value, BASE);
+            out += numToChar(r);
+            value = q
+        }
+    }
+
+    // if (bits > 0) {
+    //     out += ((bitAccum << (6 - bits)) & 0x3f)
+    // }
+
     return out
 }
 
+// console.log(charToNum('0'))
 
-const mask6 = 0b11111100
-const mask4 = 0b11110000
-const mask2 = 0b11000000
+export function base50ToBuf(base50Str: string): Uint8Array {
+    let sextets:number[] = []
+    let carry = 0
+    let divisor = 15
+
+    for(let i=0; i<base50Str.length; ++i) {
+        const num = charToNum(base50Str[i])+carry
+        if(num < 49) {
+            sextets.push(num)
+            carry = 0
+        } else {
+            ++i
+            assert(i< base50Str.length)
+            const num2 = charToNum(base50Str[i])
+            const rem = num2 % divisor;
+            sextets.push(num+rem)
+            carry = (num2-rem)/divisor;
+            // console.log(carry)
+        }
+    }
+
+    if(carry > 0) {
+        sextets.push(carry)
+    }
+
+    // console.log(sextets)
+    // console.log([...sextets2buf(sextets)])
+    return new Uint8Array(sextets2buf(sextets))
+}
+
+// console.log(numToChar(0b1111))
+console.log(base50ToBuf('Zg0Zc'))  // [0b1111_1100,0b0000_1111] == [252, 15]
+
+export function* sextets2buf(sextets: Iterable<number>) {
+    let phase = 0
+    let carry = 0
+
+    for(const val of sextets) {
+        switch(phase) {
+            case 0:
+                carry = val
+                phase = 1
+                break
+            case 1:
+                // 6 bits in the carry, want the first 2 bits of val
+                yield ((carry << 2) | (val >> 4))
+                carry = val & 0b0000_1111
+                phase = 2
+                break
+            case 2:
+                // 4 bits in the carry, want the first 4 bits of val
+                // console.log(carry<<4,val)
+                yield ((carry << 4) | (val >> 2))
+                carry = val & 0b0000_0011
+                phase = 3
+                break
+            case 3:
+                // 2 bits in the carry, want all 6 bits of val
+                yield ((carry << 6) | val)
+                phase = 0
+                break
+        }
+    }
+    if(carry !== 0) {
+        yield carry
+    }
+}
+
 
 export function* chunk6bits(buf: Iterable<number>) {
     let phase = 0
