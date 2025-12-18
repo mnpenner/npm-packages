3d444c50bee47a3bc482c2db98c1f85bbb12e9b0
Mark Penner <code@mpen.ca>
2025-12-18 00:13:07 -0800

    gen1

--- packages/rerouter/packages/server-router/examples/serve.ts
+++ packages/rerouter/packages/server-router/examples/serve.ts
@@ -2,4 +2,8 @@
 import {router} from './router-instance'
 
 // BUN_PORT=3001 bun packages/server-router/examples/serve.ts
-export default router
+// export default router
+
+
+const server = Bun.serve({ port: 5180, fetch: router.fetch.bind(router) });
+console.log(`server-router running at http://localhost:${server.port}`);
