0d9753ace85b44d62f3721cd0eac5ab5d41e68c3
Mark Penner <code@mpen.ca>
2026-04-24 16:12:23 -0700

    Update podman repo url

--- packages/podman/dist/lib/arg-builder.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class ArgBuilder {
-    private readonly args;
-    constructor(command: string);
-    addValue(flag: string, value?: string | number): void;
-    addBool(flag: string, value?: boolean): void;
-    addValues(flag: string, values?: string | string[]): void;
-    add(arg: string): void;
-    toArgs(): string[];
-}
