892caf374f53e34fe04092ca70adb4a172d63e85
Mark Penner <code@mpen.ca>
2026-05-03 01:34:53 -0700

    Freshen tsconfig paths

--- scripts/tspaths.ts
+++ scripts/tspaths.ts
@@ -4,7 +4,8 @@ import { $ } from 'bun'
 import { applyEdits, modify } from 'jsonc-parser'
 import { readFileSync } from 'fs'
 import fg from 'fast-glob'
-import { dirname, join, resolve,relative } from 'node:path/posix'
+import { dirname, join, resolve, relative } from 'node:path/posix'
+import { writeFileSync } from 'node:fs'
 
 const PARSE_CONFIG = {
     options: {},
@@ -15,29 +16,24 @@ const PARSE_CONFIG = {
 async function main(options: Options, positionals: Positionals): Promise<number | void> {
     const root = resolve(__dirname, '..')
 
+    const tsconfigPath = 'tsconfig.json'
+    let text = readFileSync(tsconfigPath, 'utf8')
 
-    const path = "tsconfig.json";
-    const text = readFileSync(path, "utf8");
-
-// parse
-//     const data = parse(text);
+    // parse
+    //     const data = parse(text);
 
     // const edits = modify(text, ['compilerOptions','paths','@mpen/base50'],['ooga booga'],{})
 
-
     const formattingOptions = {
-        insertSpaces: true,
-        tabSize: 2,
+        // insertSpaces: true,
+        // tabSize: 2,
     }
-
 
     const tsdowns = await fg('packages/*/tsdown.config.ts')
 
-    let edits: ReturnType<typeof modify> = []
-
-
-    for(const tsdown of tsdowns) {
-        let entry = await import(tsdown).then(m => m.default.entry)
+    // WARNING! This won't work if the entry uses globs or something. tsdown supports complicated entries... https://tsdown.dev/options/entry if this fails we might consider tapping into whatever voodoo they do
+    for (const tsdown of tsdowns) {
+        let entry = await import(tsdown).then((m) => m.default.entry)
         const dir = dirname(tsdown)
         const pkgPath = join(dir, 'package.json')
 
@@ -45,61 +41,81 @@ async function main(options: Options, positionals: Positionals): Promise<number 
 
         // console.log(pkg.name)
 
-        function resolveEntry(entry: string|string[]) {
+        function resolveEntry(entry: string | string[]) {
             if (!Array.isArray(entry)) entry = [entry]
 
-            return entry.map(e => relative(root, join(dir, e)))
+            return entry.map((e) => relative(root, join(dir, e)))
         }
 
-        if(Array.isArray(entry) || typeof entry === 'string') {
-
-            edits = edits.concat(
+        if (Array.isArray(entry) || typeof entry === 'string') {
+            text = applyEdits(
+                text,
                 modify(text, ['compilerOptions', 'paths', pkg.name], resolveEntry(entry), {
                     formattingOptions,
                 }),
             )
-        } else if(typeof entry === 'object') {
-
-            for(const [key, value] of Object.entries(entry)) {
-
-                const pkgPath = join(pkg.name, key)
-
-                edits = edits.concat(
-                    modify(text, ['compilerOptions', 'paths', pkgPath], resolveEntry(value), {
-                        formattingOptions,
-                    }),
+        } else if (typeof entry === 'object') {
+            const values = Object.values(entry)
+            if (values.length === 1) {
+                text = applyEdits(
+                    text,
+                    modify(
+                        text,
+                        ['compilerOptions', 'paths', pkg.name],
+                        resolveEntry(values[0] as string),
+                        {
+                            formattingOptions,
+                        },
+                    ),
                 )
+            } else {
+                for (const [key, value] of Object.entries(entry)) {
+                    const pkgPath = key === 'index' ? pkg.name : join(pkg.name, key)
+
+                    text = applyEdits(
+                        text,
+                        modify(
+                            text,
+                            ['compilerOptions', 'paths', pkgPath],
+                            resolveEntry(value as string),
+                            {
+                                formattingOptions,
+                            },
+                        ),
+                    )
+                }
             }
         } else {
-            console.error(`Invalid entry for ${pkg.name}`,entry)
+            console.error(`Invalid entry for ${pkg.name}`, entry)
         }
 
         // console.log(config)
     }
 
-    const updated = applyEdits(text, edits)
+    // console.log(text)
+    // const updated = applyEdits(text, edits)
 
-    console.log(updated)
+    writeFileSync(tsconfigPath, text)
 
     return 0
 }
 
 //#region Invoke main
 type ParsedConfig = ReturnType<typeof parseArgs<typeof PARSE_CONFIG>>
-type Options = ParsedConfig["values"]
-type Positionals = ParsedConfig["positionals"]
+type Options = ParsedConfig['values']
+type Positionals = ParsedConfig['positionals']
 
-if(import.meta.main) {
-    const {values, positionals} = parseArgs(PARSE_CONFIG)
+if (import.meta.main) {
+    const { values, positionals } = parseArgs(PARSE_CONFIG)
 
     Promise.try(main, values, positionals).then(
         (exitCode) => {
-            if(typeof exitCode === "number") {
+            if (typeof exitCode === 'number') {
                 process.exitCode = exitCode
             }
         },
         (err) => {
-            if(err instanceof $.ShellError) {
+            if (err instanceof $.ShellError) {
                 console.error(`Command failed with exit code ${err.exitCode}`)
                 process.exitCode = err.exitCode
             } else {
