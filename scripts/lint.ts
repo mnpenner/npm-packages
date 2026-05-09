62a7df8068a17bd61cce9cf374e7d6996c630ee6
Mark Penner <code@mpen.ca>
2026-05-09 13:21:49 -0700

    lint paths

--- scripts/lint.ts
+++ scripts/lint.ts
@@ -1,11 +1,18 @@
 #!/usr/bin/env -S bun -i
 import { $ } from 'bun'
 import { resolvePackagePathTargets } from './lib/package-dirs'
+import { sh } from './lib/shell-exec.ts'
+import { relative, resolve } from 'node:path'
+
+function resolveScript(path: string) {
+    return relative(process.cwd(), resolve(__dirname, path)).replaceAll('\\', '/')
+}
 
 async function main(args: string[]): Promise<number | void> {
     const targets = await resolvePackagePathTargets(args.length > 0 ? args : ['.'])
+    const formatter = resolveScript('lib/eslint-relative-formatter.ts')
 
-    await $`bun run --bun eslint --format ./scripts/eslint-relative-formatter.ts ${targets}`
+    await sh`bun run --bun eslint --format ${formatter} ${targets}`
 }
 
 //#region Invoke main
