dd04db3a04df88646a65bf084c22abae4f732579
Mark Penner <code@mpen.ca>
2026-05-11 00:27:12 -0700

    Experimenting with server-router

--- packages/server-router/example3/test-gen.ts
+++ packages/server-router/example3/test-gen.ts
@@ -1,15 +1,20 @@
 #!/usr/bin/env -S bun
+import { expectType } from '@mpen/ts-types'
 import { ApiClient, type Fetcher } from './router.gen'
 
 class FakeFetcher implements Fetcher {
     fetch(url: string, init: RequestInit): Promise<Response> {
         console.log('fetching', url, init)
-        return Promise.resolve(new Response())
+        return Promise.resolve(new Response(JSON.stringify({"error":"msg"}), { status: 400 }))
     }
 }
 
 const client = new ApiClient(new FakeFetcher())
 
-client.widgets.byId
+const res = await client.widgets.byId
     .post(123, { view: 'full' }, { name: 'Mark', tags: ['foo', 'bar'] })
-    .then(console.log)
+
+if(res.status === 400) {
+    const body = await res.json()
+    expectType<string>(body.component)
+}
