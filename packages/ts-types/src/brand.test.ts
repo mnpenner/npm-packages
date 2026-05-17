4e73d174ffe0a2b441f754323d4b7924d6d0d9be
Mark Penner <code@mpen.ca>
2026-05-17 00:55:51 -0700

    Branded

---
+++ packages/ts-types/src/brand.test.ts
@@ -0,0 +1,19 @@
+import { makeBrand, type Branded } from './brand.ts'
+
+type PortNumber = Branded<number, 'port'>
+
+const PortNumber = makeBrand<PortNumber>()
+
+const port = PortNumber(3000)
+
+// @ts-expect-error Should reject incorrect type
+const badPort = PortNumber('not a number')
+
+///
+
+const UserId = makeBrand<number,'UserId'>()
+
+const userId = UserId(123)
+
+// @ts-expect-error Should reject incorrect type
+const badUser = UserId('bad')
---
+++ packages/ts-types/src/brand.ts
@@ -0,0 +1,44 @@
+declare const BRAND: unique symbol
+
+/**
+ * @experimental
+ */
+export type Branded<TData, TBrand> = TData & {
+    readonly [BRAND]: {
+        readonly data: TData
+        readonly brand: TBrand
+    }
+}
+
+/**
+ * @experimental
+ */
+export interface AnyBranded {
+    readonly [BRAND]: {
+        readonly data: unknown
+        readonly brand: unknown
+    }
+}
+
+/**
+ * @experimental
+ */
+export type BrandData<T extends AnyBranded> = T[typeof BRAND]['data']
+
+/**
+ * @experimental
+ */
+export function makeBrand<TBranded extends AnyBranded>(): (
+    value: BrandData<TBranded>,
+) => TBranded
+
+/**
+ * @experimental
+ */
+export function makeBrand<TData, TBrand>(): (
+    value: TData,
+) => Branded<TData, TBrand>
+
+export function makeBrand() {
+    return (value: unknown) => value as never
+}
